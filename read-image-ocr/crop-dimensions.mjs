4292fa54e497531f6cdcdc44fc54c4f77ea58ea4
Mike Hsu <mike.hsu@gmail.com>
2018-02-24 00:18:59 -0800

    first try at ocr

---
+++ read-image-ocr/crop-dimensions.mjs
@@ -0,0 +1,74 @@
+export default [
+    {
+      name: 'mode',
+        x: 220,
+        y: 423,
+        w: 124,
+        h: 77,
+      tessOpts: {
+        lang: 'splatoon2',
+      }
+    },{
+      name: 'player1Weapon',
+        x: 676,
+        y: 104,
+        w: 46,
+        h: 38,
+
+    },{
+      name: 'player1Rank',
+        x: 645,
+        y: 115,
+        w: 34,
+        h: 24,
+      },{
+      name: 'player1Name',
+        x: 719,
+        y: 102,
+        w: 139,
+        h: 22,
+        tessOpts: {
+          lang: 'splatoon2'
+        }
+    },{
+      name: 'player1KA',
+        x: 860,
+        y: 116,
+        w: 37,
+        h: 20,
+        tessOpts: {
+          lang: 'splatoon2',
+          tessedit_char_whitelist: '0123456789()'
+        }
+    },{
+      name: 'player1D',
+        x: 903,
+        y: 114,
+        w: 26,
+        h: 20,
+        tessOpts: {
+          lang: 'splatoon2',
+          tessedit_char_whitelist: '0123456789'
+        }
+    },{
+      name: 'player1S',
+        x: 937,
+        y: 113,
+        w: 22,
+        h: 20,
+        tessOpts: {
+          lang: 'splatoon2',
+          tessedit_char_whitelist: '0123456789'
+        }
+    },{
+      name: 'player1P',
+        x: 719,
+        y: 122,
+        w: 47,
+        h: 17,
+        tessOpts: {
+          lang: 'splatoon2',
+          tessedit_char_whitelist: '0123456789p'
+        }
+    }
+];
--- read-image-ocr/index.mjs
+++ read-image-ocr/index.mjs
@@ -1,58 +1,18 @@
 import cv from 'opencv';
+import Tesseract from 'tesseract.js';
+import cropDimensions from './crop-dimensions';
+import path from 'path';
 
-const crops = {
-    mode: {
-        x: 220,
-        y: 423,
-        w: 124,
-        h: 77,
-    },
-    player1Weapon: {
-        x: 676,
-        y: 104,
-        w: 46,
-        h: 38,
-    },
-    player1Rank: {
-        x: 645,
-        y: 115,
-        w: 34,
-        h: 24,
-    },
-    player1Name: {
-        x: 719,
-        y: 102,
-        w: 139,
-        h: 22,
-    },
-    player1KA: {
-        x: 860,
-        y: 116,
-        w: 37,
-        h: 20,
-    },
-    player1D: {
-        x: 903,
-        y: 114,
-        w: 26,
-        h: 20,
-    },
-    player1S: {
-        x: 937,
-        y: 113,
-        w: 22,
-        h: 20,
-    },
-    player1P: {
-        x: 719,
-        y: 122,
-        w: 47,
-        h: 17,
+const langPath = `${path.join(path.resolve('.'), './tessdata')}`;
+const Tact = Tesseract.create({ langPath });
+cv.readImage('./test-images/en-SZ-blackbelly-league.jpg', async (err, im) => {
+  for (const d of cropDimensions) {
+    const crop = im.crop(d.x, d.y, d.w, d.h);
+    const filename = `./output/${d.name}.png`;
+    crop.save(filename);
+    if (d.tessOpts != null) {
+      const result = await Tact.recognize(filename, d.tessOpts);
+      console.log(`${d.name}(${result.confidence}): ${result.text.trim()}`);
     }
-}
-
-cv.readImage('./test-images/en-SZ-blackbelly-league.jpg', (err, im) => {
-    const name = 'player1S';
-    const tempCrop = im.crop(crops[name].x, crops[name].y, crops[name].w, crops[name].h);
-    tempCrop.save('./output/out.png');
+  }
 });
